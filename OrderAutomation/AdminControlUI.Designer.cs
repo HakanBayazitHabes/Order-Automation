73e2452336ef5f3c3d47e101bf8a1d1cbb870424
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2022-07-26 16:48:52 +0300

    order automation project

---
+++ OrderAutomation/AdminControlUI.Designer.cs
@@ -0,0 +1,337 @@
+﻿
+namespace OrderAutomation
+{
+    partial class AdminControlUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.tbxName = new System.Windows.Forms.TextBox();
+            this.tbxQuantity = new System.Windows.Forms.TextBox();
+            this.tbxStatus = new System.Windows.Forms.TextBox();
+            this.tbxAddress = new System.Windows.Forms.TextBox();
+            this.tbxItemName = new System.Windows.Forms.TextBox();
+            this.lblItemName = new System.Windows.Forms.Label();
+            this.tbxPrice = new System.Windows.Forms.TextBox();
+            this.tbxDescription = new System.Windows.Forms.TextBox();
+            this.tbxShippingWeight = new System.Windows.Forms.TextBox();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.lblShippingWeight = new System.Windows.Forms.Label();
+            this.gbxItem = new System.Windows.Forms.GroupBox();
+            this.lblPayType = new System.Windows.Forms.Label();
+            this.tbxPayType = new System.Windows.Forms.TextBox();
+            this.dgwAdmin = new System.Windows.Forms.DataGridView();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.gbxItem.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwAdmin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(100, 49);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(62, 18);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(81, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Quantity :";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(94, 130);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(66, 18);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "Status :";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(108, 206);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(53, 18);
+            this.lblDate.TabIndex = 4;
+            this.lblDate.Text = "Date :";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(81, 256);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(79, 18);
+            this.lblAddress.TabIndex = 5;
+            this.lblAddress.Text = "Address :";
+            // 
+            // tbxName
+            // 
+            this.tbxName.Location = new System.Drawing.Point(193, 48);
+            this.tbxName.Name = "tbxName";
+            this.tbxName.Size = new System.Drawing.Size(222, 24);
+            this.tbxName.TabIndex = 6;
+            // 
+            // tbxQuantity
+            // 
+            this.tbxQuantity.Location = new System.Drawing.Point(193, 85);
+            this.tbxQuantity.Name = "tbxQuantity";
+            this.tbxQuantity.Size = new System.Drawing.Size(222, 24);
+            this.tbxQuantity.TabIndex = 7;
+            // 
+            // tbxStatus
+            // 
+            this.tbxStatus.Location = new System.Drawing.Point(193, 127);
+            this.tbxStatus.Name = "tbxStatus";
+            this.tbxStatus.Size = new System.Drawing.Size(222, 24);
+            this.tbxStatus.TabIndex = 9;
+            // 
+            // tbxAddress
+            // 
+            this.tbxAddress.Location = new System.Drawing.Point(193, 254);
+            this.tbxAddress.Name = "tbxAddress";
+            this.tbxAddress.Size = new System.Drawing.Size(667, 24);
+            this.tbxAddress.TabIndex = 11;
+            // 
+            // tbxItemName
+            // 
+            this.tbxItemName.Location = new System.Drawing.Point(220, 32);
+            this.tbxItemName.Name = "tbxItemName";
+            this.tbxItemName.Size = new System.Drawing.Size(188, 24);
+            this.tbxItemName.TabIndex = 7;
+            // 
+            // lblItemName
+            // 
+            this.lblItemName.AutoSize = true;
+            this.lblItemName.Location = new System.Drawing.Point(79, 35);
+            this.lblItemName.Name = "lblItemName";
+            this.lblItemName.Size = new System.Drawing.Size(99, 18);
+            this.lblItemName.TabIndex = 6;
+            this.lblItemName.Text = "Item Name :";
+            // 
+            // tbxPrice
+            // 
+            this.tbxPrice.Location = new System.Drawing.Point(220, 157);
+            this.tbxPrice.Name = "tbxPrice";
+            this.tbxPrice.Size = new System.Drawing.Size(188, 24);
+            this.tbxPrice.TabIndex = 5;
+            // 
+            // tbxDescription
+            // 
+            this.tbxDescription.Location = new System.Drawing.Point(220, 115);
+            this.tbxDescription.Name = "tbxDescription";
+            this.tbxDescription.Size = new System.Drawing.Size(188, 24);
+            this.tbxDescription.TabIndex = 4;
+            // 
+            // tbxShippingWeight
+            // 
+            this.tbxShippingWeight.Location = new System.Drawing.Point(220, 74);
+            this.tbxShippingWeight.Name = "tbxShippingWeight";
+            this.tbxShippingWeight.Size = new System.Drawing.Size(188, 24);
+            this.tbxShippingWeight.TabIndex = 3;
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(129, 159);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(57, 18);
+            this.lblPrice.TabIndex = 2;
+            this.lblPrice.Text = "Price :";
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(77, 118);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(104, 18);
+            this.lblDescription.TabIndex = 1;
+            this.lblDescription.Text = "Description :";
+            // 
+            // lblShippingWeight
+            // 
+            this.lblShippingWeight.AutoSize = true;
+            this.lblShippingWeight.Location = new System.Drawing.Point(13, 76);
+            this.lblShippingWeight.Name = "lblShippingWeight";
+            this.lblShippingWeight.Size = new System.Drawing.Size(164, 18);
+            this.lblShippingWeight.TabIndex = 0;
+            this.lblShippingWeight.Text = "ShippingWeight(kg) :";
+            // 
+            // gbxItem
+            // 
+            this.gbxItem.Controls.Add(this.tbxItemName);
+            this.gbxItem.Controls.Add(this.lblItemName);
+            this.gbxItem.Controls.Add(this.tbxPrice);
+            this.gbxItem.Controls.Add(this.tbxDescription);
+            this.gbxItem.Controls.Add(this.tbxShippingWeight);
+            this.gbxItem.Controls.Add(this.lblPrice);
+            this.gbxItem.Controls.Add(this.lblDescription);
+            this.gbxItem.Controls.Add(this.lblShippingWeight);
+            this.gbxItem.Location = new System.Drawing.Point(428, 36);
+            this.gbxItem.Name = "gbxItem";
+            this.gbxItem.Size = new System.Drawing.Size(433, 196);
+            this.gbxItem.TabIndex = 19;
+            this.gbxItem.TabStop = false;
+            this.gbxItem.Text = "Seçilem Ürün";
+            // 
+            // lblPayType
+            // 
+            this.lblPayType.AutoSize = true;
+            this.lblPayType.Location = new System.Drawing.Point(76, 169);
+            this.lblPayType.Name = "lblPayType";
+            this.lblPayType.Size = new System.Drawing.Size(87, 18);
+            this.lblPayType.TabIndex = 20;
+            this.lblPayType.Text = "Pay Type :";
+            // 
+            // tbxPayType
+            // 
+            this.tbxPayType.Location = new System.Drawing.Point(193, 166);
+            this.tbxPayType.Name = "tbxPayType";
+            this.tbxPayType.Size = new System.Drawing.Size(222, 24);
+            this.tbxPayType.TabIndex = 21;
+            // 
+            // dgwAdmin
+            // 
+            this.dgwAdmin.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgwAdmin.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgwAdmin.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwAdmin.Location = new System.Drawing.Point(84, 354);
+            this.dgwAdmin.Name = "dgwAdmin";
+            this.dgwAdmin.Size = new System.Drawing.Size(777, 252);
+            this.dgwAdmin.TabIndex = 26;
+            this.dgwAdmin.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgwAdmin_CellClick);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.Yellow;
+            this.btnUpdate.ForeColor = System.Drawing.Color.Black;
+            this.btnUpdate.Location = new System.Drawing.Point(84, 297);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(199, 44);
+            this.btnUpdate.TabIndex = 27;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDelete.ForeColor = System.Drawing.Color.GhostWhite;
+            this.btnDelete.Location = new System.Drawing.Point(664, 297);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(197, 44);
+            this.btnDelete.TabIndex = 28;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Enabled = false;
+            this.dtpDate.Location = new System.Drawing.Point(193, 206);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(222, 24);
+            this.dtpDate.TabIndex = 29;
+            // 
+            // AdminControlUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(953, 642);
+            this.Controls.Add(this.dtpDate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.dgwAdmin);
+            this.Controls.Add(this.tbxPayType);
+            this.Controls.Add(this.lblPayType);
+            this.Controls.Add(this.gbxItem);
+            this.Controls.Add(this.tbxAddress);
+            this.Controls.Add(this.tbxStatus);
+            this.Controls.Add(this.tbxQuantity);
+            this.Controls.Add(this.tbxName);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblName);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "AdminControlUI";
+            this.Text = "Admin Control";
+            this.Load += new System.EventHandler(this.AdminControlUI_Load);
+            this.gbxItem.ResumeLayout(false);
+            this.gbxItem.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwAdmin)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.TextBox tbxName;
+        private System.Windows.Forms.TextBox tbxQuantity;
+        private System.Windows.Forms.TextBox tbxStatus;
+        private System.Windows.Forms.TextBox tbxAddress;
+        private System.Windows.Forms.TextBox tbxItemName;
+        private System.Windows.Forms.Label lblItemName;
+        private System.Windows.Forms.TextBox tbxPrice;
+        private System.Windows.Forms.TextBox tbxDescription;
+        private System.Windows.Forms.TextBox tbxShippingWeight;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.Label lblShippingWeight;
+        private System.Windows.Forms.GroupBox gbxItem;
+        private System.Windows.Forms.Label lblPayType;
+        private System.Windows.Forms.TextBox tbxPayType;
+        private System.Windows.Forms.DataGridView dgwAdmin;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+    }
+}
---
+++ OrderAutomation/CustomerUI.cs
@@ -0,0 +1,272 @@
+﻿using OrderAutomation.Business.Abstract;
+using OrderAutomation.Business.Concrete;
+using OrderAutomation.DataAccsessLayer.Concrete.EntityFrameWork;
+using OrderAutomation.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrderAutomation
+{
+    public partial class CustomerUI : Form
+    {
+        public CustomerUI()
+        {
+            InitializeComponent();
+            _cashServices = new CashManager(new EfCashDal());
+            _checkServices = new CheckManager(new EfCheckDal());
+            _creditServices = new CreditManager(new EfCreditDal());
+            _customerChooseServices = new CustomerChooseManager(new EfCustomerChooseDal());
+            _customerServices = new CustomerManager(new EfCustomerDal());
+            _itemServices = new ItemManager(new EfItemDal());
+            _orderDetailServices = new OrderDetailManager(new EfOrderDetailDal());
+            _orderServices = new OrderManager(new EfOrderDal());
+        }
+        public static string PalType;
+
+        private ICashServices _cashServices;
+        private ICheckServices _checkServices;
+        private ICreditServices _creditServices;
+        private ICustomerChooseServices _customerChooseServices;
+        private ICustomerServices _customerServices;
+        private IItemServices _itemServices;
+        private IOrderDetailServices _orderDetailServices;
+        private IOrderServices _orderServices;
+
+        public CustomerUI(IOrderServices orderServices)
+        {
+            _orderServices = orderServices;
+        }
+
+        public CustomerUI(IOrderDetailServices orderDetailServices)
+        {
+            _orderDetailServices = orderDetailServices;
+        }
+
+        public CustomerUI(IItemServices ıtemServices)
+        {
+            _itemServices = ıtemServices;
+        }
+
+        public CustomerUI(ICustomerServices customerServices)
+        {
+            _customerServices = customerServices;
+        }
+
+        public CustomerUI(ICustomerChooseServices customerChooseServices)
+        {
+            _customerChooseServices = customerChooseServices;
+        }
+
+        public CustomerUI(ICreditServices creditServices)
+        {
+            _creditServices = creditServices;
+        }
+
+        public CustomerUI(ICheckServices checkServices)
+        {
+            _checkServices = checkServices;
+        }
+
+        public CustomerUI(ICashServices cashServices)
+        {
+            _cashServices = cashServices;
+        }
+
+        private void CustomerUI_Load(object sender, EventArgs e)
+        {
+            LoadItems();
+            gbxCash.Enabled = false;
+            gbxCheck.Enabled = false;
+            gbxCredit.Enabled = false;
+        }
+
+        private void LoadItems()
+        {
+            dgwItem.DataSource = _itemServices.GetAll();
+            PalType = cbxPayType.Text;
+        }
+
+        private void btnGiveanOrder_Click(object sender, EventArgs e)
+        {
+            if (tbxQuantity.Text != "" && tbxTax.Text != "" && tbxName.Text != "" && tbxAddress.Text != "" && tbxItemName.Text != "" && tbxShippingWeight.Text != "" && cbxPayType.Text != "" && tbxStatus.Text != ""  && tbxDescription.Text != "" && tbxPrice.Text != "")
+            {
+                _customerServices.Add(new Customer
+                {
+                    Name = tbxName.Text,
+                    Address = tbxAddress.Text,
+                });
+            }
+            else
+            {
+                MessageBox.Show("Lütfen Boş Alanları Doldurunuz!");
+            }
+           
+
+            if (tbxQuantity.Text != "" && tbxTax.Text != "" && tbxName.Text != "" && tbxAddress.Text != "" && tbxItemName.Text != "" && tbxShippingWeight.Text != "" && cbxPayType.Text != "" && tbxStatus.Text != ""  && tbxDescription.Text != "" && tbxPrice.Text != "")
+            {
+                _orderDetailServices.Add(new OrderDetail
+                {
+                    Quantity = Convert.ToInt32(tbxQuantity.Text),
+                    taxStatus = Convert.ToInt32(tbxTax.Text)
+                });
+            }
+            else
+            {
+                MessageBox.Show("Lütfen Boş Alanları Doldurunuz!");
+            }
+
+            if (tbxQuantity.Text != "" && tbxTax.Text != "" && tbxName.Text != "" && tbxAddress.Text != "" && tbxItemName.Text != "" && tbxShippingWeight.Text != "" && cbxPayType.Text != "" && tbxStatus.Text != ""  && tbxDescription.Text != "" && tbxPrice.Text != "")
+            {
+                _orderServices.Add(new Order
+                {
+                    Status = tbxStatus.Text,
+                    Date = Convert.ToDateTime(dtpDate.Value.ToShortDateString())
+                });
+            }
+
+            if (tbxQuantity.Text != "" && tbxTax.Text != "" && tbxName.Text != "" && tbxAddress.Text != "" && tbxItemName.Text != "" && tbxShippingWeight.Text != "" && cbxPayType.Text != "" && tbxStatus.Text != ""  && tbxDescription.Text != "" && tbxPrice.Text != "")
+            {
+                _customerChooseServices.Add(new CustomerChoose
+                {
+                    CItemName = tbxItemName.Text,
+                    CShippingWeight = Convert.ToDouble(tbxShippingWeight.Text),
+                    CDescription = tbxDescription.Text,
+                    CPrice = Convert.ToDecimal(tbxPrice.Text),
+                    PayType = cbxPayType.Text
+                });
+            }
+            else
+            {
+                MessageBox.Show("Lütfen listeden bir ürün ve ödeme türü seçiniz.");
+            }
+
+
+            if (cbxPayType.Text == "Cash")
+            {
+                if (tbxQuantity.Text != "" && tbxTax.Text != "" && tbxName.Text != "" && tbxAddress.Text != "" && tbxItemName.Text != "" && tbxShippingWeight.Text != "" && cbxPayType.Text != "" && tbxStatus.Text != ""  && tbxDescription.Text != "" && tbxPrice.Text != "")
+                {
+                    _cashServices.Add(new Cash
+                    {
+                        CashTendered = Convert.ToDouble(tbxCashTendered.Text)
+                    });
+                }
+                else
+                {
+                    MessageBox.Show("Lütfen kart bilgilerinizi giiriniz.");
+                }
+
+            }
+             if (cbxPayType.Text=="Check")
+            {
+                if (tbxQuantity.Text != "" && tbxTax.Text != "" && tbxName.Text != "" && tbxAddress.Text != "" && tbxItemName.Text != "" && tbxShippingWeight.Text != "" && cbxPayType.Text != "" && tbxStatus.Text != ""  && tbxDescription.Text != "" && tbxPrice.Text != "")
+                {
+                    _checkServices.Add(new Check
+                    {
+                        Name = tbxCheckName.Text,
+                        BankID = tbxCheckBankID.Text,
+                        Amount = Convert.ToDecimal(tbxCheckAmount.Text)
+                    });
+                }
+                else
+                {
+                    MessageBox.Show("Lütfen kart bilgilerinizi giiriniz.");
+                }
+                
+            }
+            if (cbxPayType.Text == "Credit")
+            {
+                if (tbxQuantity.Text != "" && tbxTax.Text != "" && tbxName.Text != "" && tbxAddress.Text != "" && tbxItemName.Text != "" && tbxShippingWeight.Text != "" && cbxPayType.Text != "" && tbxStatus.Text != ""  && tbxDescription.Text != "" && tbxPrice.Text != "")
+                {
+                    _creditServices.Add(new Credit
+                    {
+                        Number = tbxCreditNumber.Text,
+                        Type = cbxCreditType.Text,
+                        ExpData = tbxCreditDate.Text,
+                        Amount = Convert.ToDecimal(tbxCreditAmount.Text)
+                    });
+                }
+                else
+                {
+                    MessageBox.Show("Lütfen kart bilgilerinizi giiriniz.");
+                }
+            }
+            //else
+            //{
+            //    MessageBox.Show("Lütfen bir ödeme türü seçiniz.");
+            //}
+            if (tbxQuantity.Text != "" && tbxTax.Text != "" && tbxName.Text != "" && tbxAddress.Text != "" && tbxItemName.Text != "" && tbxShippingWeight.Text != "" && cbxPayType.Text != "")
+                MessageBox.Show("Your Order has been Received");
+        }
+        private void cbxPayType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            if (cbxPayType.Text == "Cash")
+            {
+                tbxCashTendered.Text = label2.Text;
+                gbxCash.Enabled = true;
+                gbxCheck.Enabled = false;
+                gbxCredit.Enabled = false;
+            }
+            if (cbxPayType.Text == "Check")
+            {
+                tbxCheckAmount.Text = label2.Text;
+                gbxCash.Enabled = false;
+                gbxCheck.Enabled = true;
+                gbxCredit.Enabled = false;
+            }
+            if (cbxPayType.Text == "Credit")
+            {
+                tbxCreditAmount.Text = label2.Text;
+                gbxCash.Enabled = false;
+                gbxCheck.Enabled = false;
+                gbxCredit.Enabled = true;
+            }
+
+        }
+
+        private void dgwItem_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var row = dgwItem.CurrentRow;
+            tbxShippingWeight.Text = row.Cells[1].Value.ToString();
+            tbxDescription.Text = row.Cells[2].Value.ToString();
+            tbxPrice.Text = row.Cells[3].Value.ToString();
+            tbxItemName.Text = row.Cells[4].Value.ToString();
+        }
+
+        private void tbxQuantity_TextChanged(object sender, EventArgs e)
+        {
+            int tbxprice = Convert.ToInt32(dgwItem.CurrentRow.Cells[3].Value);
+            float tbxweight = Convert.ToInt32(dgwItem.CurrentRow.Cells[1].Value);
+
+            int price=0;
+            float weight = 0;
+            int tax = 0;
+            if (tbxQuantity.Text == "")
+            {
+                tbxQuantity.Text = "1";
+
+            }
+            price = _itemServices.GetPriceForWuantity(Convert.ToInt32(tbxQuantity.Text), tbxprice);
+            weight = _itemServices.GetWeightForQuantity(Convert.ToInt32(tbxQuantity.Text), tbxweight);
+            tax = _orderServices.GetTax(tbxprice, Convert.ToInt32(tbxQuantity.Text));
+            
+            tbxPrice.Text = price.ToString();
+            tbxShippingWeight.Text = weight.ToString();
+            tbxTax.Text = tax.ToString();
+            label2.Text = Convert.ToString(price + tax);
+            //if (cbxPayType.Text == "Cash")
+            tbxCashTendered.Text = label2.Text;
+            //if (cbxPayType.Text == "Check")
+                tbxCheckAmount.Text = label2.Text;
+            //if (cbxPayType.Text == "Credit")
+                tbxCreditAmount.Text = label2.Text;
+        }
+    }
+}
